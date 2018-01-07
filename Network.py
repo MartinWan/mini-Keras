33f5268dd3d06c1e54de11742777e5c869d3c12b
Martin Wan <mlwan@uvic.ca>
2018-01-07 08:32:24 -0800

    basic functionality of vanilla ANN

---
+++ Network.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+def ReLu(x):
+    return np.maximum(x, 0)
+
+def ReLuprime(x):
+    return np.where(x > 0, 1.0, 0.0)
+
+class DenseNetwork(object):
+    """ Basic neural network with fully connected layers
+    # Arguments
+        layers: a list of integers such that layers[l] = neurons in layer l
+        sigma: activation function
+        sigmaprime: derivative of activation function
+    # References
+        - [Neural Networks and Deep Learning](http://neuralnetworksanddeeplearning.com/)
+    """
+    def __init__(self, layers, sigma=ReLu, sigmaprime=ReLuprime):
+        self.L = len(layers)
+        self.sigma = sigma
+        self.sigmaprime = sigmaprime
+        self.weights = []
+        self.biases = []
+        for l in range(1, self.L):
+            self.weights.append(np.random.rand(layers[l], layers[l-1]))
+            self.biases.append(np.random.rand(layers[l], 1))
+
+    def propagate(self, x):
+        activation = x
+        for w, b in zip(self.weights, self.biases):
+            activation = self.sigma(np.dot(w, activation) + b)
+        return activation
+    
+    def backpropagate(self, x, y):
+        delCW = [np.zeros(w.shape) for w in self.weights]
+        delCB = [np.zeros(b.shape) for b in self.biases]
+
+        Z = []
+        A = [x]
+        a = x
+        for w, b in zip(self.weights, self.biases):
+            z = np.dot(w, a) + b
+            a = self.sigma(z)
+            A.append(a)
+            Z.append(z)
+
+        delta = (a - y) * self.sigmaprime(z) 
+        delCW[-1] = np.dot(delta, A[-2].T)
+        delCB[-1] = delta.sum(axis=1, keepdims=True)
+
+        for l in range(2, self.L):
+            delta = np.dot(self.weights[-l+1].T, delta) * self.sigmaprime(Z[-l])
+            delCW[-l] = np.dot(delta, A[-l-1].T)
+            delCB[-l] = delta.sum(axis=1, keepdims=True)
+
+        return delCW, delCB
+    
+    def SGD(self, X, Y):
+        lr = 0.01 # learning rate
+        for x, y in zip(X, Y):
+            delCW, delCB = self.backpropagate(x, y)
+            self.weights = [w - dw * lr for w, dw in zip(self.weights, delCW)]
+            self.biases = [b - db * lr for b, db in zip(self.biases, delCB)]
+
+
+if __name__ == '__main__':
+    '''
+        Basic test showing network learning the function
+        f(x) = np.zeros(3,1)
+    '''
+
+    net = DenseNetwork([2, 5, 3])
+    batch_size = 1000
+    x = np.random.rand(2, batch_size)
+    y = np.zeros((3, batch_size))
+
+    print '---before training---'
+    xrand = np.array([[3]
+                    ,[5]])
+    print 'f({}) = \n {}'.format(xrand, net.propagate(xrand))
+
+    print '---training...---'
+    num_epochs = 10
+    for epoch in xrange(num_epochs):
+        print 'epoch: {}...'.format(epoch+1)
+        net.SGD([x],[y])
+
+    print '---after training---'
+    for _ in xrange(1):
+        x = np.random.rand(2,1)
+        print 'f({}) = \n {}'.format(x, net.propagate(x))
+
+
+
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# mini-Keras
+
+Simple implementations of neural networks written in Python for teaching algorithms
+
+Citations:
+  [Keras Github](https://github.com/keras-team/keras)
+  [Micheal A. Nielson, "Neural Networks and Deep Learning", Determination Press, 2015](http://neuralnetworksanddeeplearning.com/)
+
