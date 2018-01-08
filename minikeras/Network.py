49762dbf7d8596dfcf3b1612bf85ddbdfd79fb2c
Martin Wan <mlwan@uvic.ca>
2018-01-07 22:29:56 -0800

    fix calculations of SGD

--- minikeras/Network.py
+++ minikeras/Network.py
@@ -1,10 +1,16 @@
 import numpy as np
 
 def ReLu(x):
-    return np.maximum(x, 0)
+    return np.maximum(x, 0.0)
 
 def ReLuprime(x):
     return np.where(x > 0, 1.0, 0.0)
+
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+def sigmoidprime(x):
+    return sigmoid(x) * (1 - sigmoid(x))
 
 class DenseNetwork(object):
     """ Basic neural network with fully connected layers
@@ -37,6 +43,7 @@ class DenseNetwork(object):
         return np.argmax(a)
 
     def backpropagate(self, x, y):
+        batch_size = x.shape[1]
         delCW = [np.zeros(w.shape) for w in self.weights]
         delCB = [np.zeros(b.shape) for b in self.biases]
 
@@ -48,37 +55,33 @@ class DenseNetwork(object):
             a = self.sigma(z)
             A.append(a)
             Z.append(z)
-
+        
         delta = (a - y) * self.sigmaprime(z) 
-        delCW[-1] = np.dot(delta, A[-2].T)
-        delCB[-1] = delta.sum(axis=1, keepdims=True) / delta.shape[1]
+        delCW[-1] = np.dot(delta, A[-2].T) / batch_size
+        delCB[-1] = delta.sum(axis=1, keepdims=True) / batch_size
 
         for l in range(2, self.L):
-            delta = np.dot(self.weights[-l+1].T, delta) * self.sigmaprime(Z[-l])
-            delCW[-l] = np.dot(delta, A[-l-1].T)
-            delCB[-l] = delta.sum(axis=1, keepdims=True)
-
+            delta = np.dot(self.weights[-l+1].T, delta) * self.sigmaprime(Z[-l]) / batch_size
+            delCW[-l] = np.dot(delta, A[-l-1].T) / batch_size
+            delCB[-l] = delta.sum(axis=1, keepdims=True) / batch_size
+        
         return delCW, delCB
     
-    def SGD(self, X, Y):
+    def SGD(self, x, y):
         lr = self.learning_rate
-        for x, y in zip(X, Y):
-            delCW, delCB = self.backpropagate(x, y)
-            self.weights = [w - dw * lr for w, dw in zip(self.weights, delCW)]
-            self.biases = [b - db * lr for b, db in zip(self.biases, delCB)]
+        delCW, delCB = self.backpropagate(x, y)
+        self.weights = [w - dw * lr for w, dw in zip(self.weights, delCW)]
+        self.biases = [b - db * lr for b, db in zip(self.biases, delCB)]
 
 
 if __name__ == '__main__':
     '''
         Basic test showing network learning the function
-        f(x) = np.ones(3,1)
+        f(x) = 2*x
     '''
 
-    net = DenseNetwork([2, 12, 13, 3], learning_rate=0.001)
-    batch_size = 1
-    x = np.random.rand(2, batch_size)
-    y = np.ones((3, batch_size))
-
+    net = DenseNetwork([2, 50, 2])
+    batch_size = 100
     print '---before training---'
     xrand = np.array([[3]
                     ,[5]])
@@ -87,11 +90,13 @@ if __name__ == '__main__':
     print '---training...---'
     num_epochs = 10000
     for epoch in xrange(num_epochs):
-        net.SGD([x],[y])
+        x = np.random.rand(2, batch_size)
+        y = 2*x
+        net.SGD(x,y)
 
     print '---after training---'
-    for _ in xrange(10):
-        x = np.random.rand(2, 1)
+    for _ in xrange(1):
+        x = np.array([[3], [5]])
         print 'outputs: {}'.format(net.propagate(x))
 
 
